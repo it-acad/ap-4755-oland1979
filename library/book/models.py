ba0376e68bc2a1aa5e84c94dcdf676d8ca17734b
Vladyslav Bondarenko <pertosel1901@gmail.com>
2026-06-07 23:09:53 +0300

    Updated models using models from previous sprint

--- library/book/models.py
+++ library/book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from author.models import Author
 
 
 class Book(models.Model):
@@ -16,17 +17,31 @@ class Book(models.Model):
         type authors: list->Author
     """
 
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    count = models.IntegerField(default=10)
+    authors = models.ManyToManyField(Author, related_name='books')
+
     def __str__(self):
         """
         Magic method is redefined to show all information about Book.
         :return: book id, book name, book description, book count, book authors
         """
+        authors_list = [author.id for author in self.authors.all()]
+        return (
+            f"'id': {self.id}, "
+            f"'name': '{self.name}', "
+            f"'description': '{self.description}', "
+            f"'count': {self.count}, "
+            f"'authors': {authors_list}"
+        )
 
     def __repr__(self):
         """
         This magic method is redefined to show class and id of Book object.
         :return: class, id
         """
+        return f"{self.__class__.__name__}(id={self.id})"
 
     @staticmethod
     def get_by_id(book_id):
@@ -34,6 +49,10 @@ class Book(models.Model):
         :param book_id: SERIAL: the id of a Book to be found in the DB
         :return: book object or None if a book with such ID does not exist
         """
+        try:
+            return Book.objects.get(id=book_id)
+        except Book.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(book_id):
@@ -42,6 +61,12 @@ class Book(models.Model):
         :type book_id: int
         :return: True if object existed in the db and was removed or False if it didn't exist
         """
+        try:
+            book = Book.objects.get(id=book_id)
+            book.delete()
+            return True
+        except Book.DoesNotExist:
+            return False
 
     @staticmethod
     def create(name, description, count=10, authors=None):
@@ -56,6 +81,24 @@ class Book(models.Model):
         type authors: list->Author
         :return: a new book object which is also written into the DB
         """
+        if len(name) > 128:
+            return None
+
+        try:
+            book = Book(
+                name=name,
+                description=description,
+                count=count
+            )
+            book.save()
+
+            if authors:
+                book.authors.add(*authors)
+
+            return book
+
+        except Exception:
+            return None
 
     def to_dict(self):
         """
@@ -69,6 +112,15 @@ class Book(models.Model):
         |   'authors': []
         | }
         """
+        authors_list = [author.id for author in self.authors.all()]
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'count': self.count,
+            'authors': authors_list
+        }
 
     def update(self, name=None, description=None, count=None):
         """
@@ -81,6 +133,19 @@ class Book(models.Model):
         type count: int default=10
         :return: None
         """
+        if name and len(name) > 128:
+            return None
+
+        if name is not None:
+            self.name = name
+
+        if description is not None:
+            self.description = description
+
+        if count is not None:
+            self.count = count
+
+        self.save()
 
     def add_authors(self, authors):
         """
@@ -88,6 +153,8 @@ class Book(models.Model):
         param authors: list authors
         :return: None
         """
+        if authors:
+            self.authors.add(*authors)
 
     def remove_authors(self, authors):
         """
@@ -95,9 +162,12 @@ class Book(models.Model):
         param authors: list authors
         :return: None
         """
+        if authors:
+            self.authors.remove(*authors)
 
     @staticmethod
     def get_all():
         """
         returns data for json request with QuerySet of all books
         """
+        return list(Book.objects.all())
--- library/order/models.py
+++ library/order/models.py
@@ -1,13 +1,42 @@
 from django.db import models
+from django.utils import timezone
+
+from authentication.models import CustomUser
+from book.models import Book
 
 
 class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField()
+    end_at = models.DateTimeField(null=True, blank=True)
+    plated_end_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         """
         Magic method is redefined to show all information about Order.
         :return: book id, book name, book description, book count, book authors
         """
+        end_at = (
+            "None"
+            if self.end_at is None
+            else f"'{self.end_at}'"
+        )
+
+        return (
+            f"'id': {self.id}, "
+            f"'user': {repr(self.user)}, "
+            f"'book': {repr(self.book)}, "
+            f"'created_at': '{self.created_at}', "
+            f"'end_at': {end_at}, "
+            f"'plated_end_at': '{self.plated_end_at}'"
+        )
 
     def __repr__(self):
         """
@@ -23,13 +52,20 @@ class Order(models.Model):
         | {
         |   'id': 8,
         |   'book': 8,
-        |   'user': 8',
+        |   'user': 8,
         |   'created_at': 1509393504,
         |   'end_at': 1509393504,
         |   'plated_end_at': 1509402866,
         | }
         """
-        pass
+        return {
+            'id': self.id,
+            'user': self.user.id,
+            'book': self.book.id,
+            'created_at': self.created_at,
+            'end_at': self.end_at,
+            'plated_end_at': self.plated_end_at,
+        }
 
     @staticmethod
     def create(user, book, plated_end_at):
@@ -42,17 +78,40 @@ class Order(models.Model):
         :type plated_end_at: int (timestamp)
         :return: a new order object which is also written into the DB
         """
+        if user.id is None:
+            return None
 
-        pass
+        if book.count <= 0:
+            return None
+
+        try:
+            order = Order(
+                user=user,
+                book=book,
+                created_at=timezone.now(),
+                plated_end_at=plated_end_at
+            )
+            order.save()
+
+            book.count -= 1
+            book.save()
+
+            return order
+
+        except Exception:
+            return None
 
     @staticmethod
     def get_by_id(order_id):
         """
         :param order_id:
         :type order_id: int
-        :return:  the object of the order, according to the specified id or null in case of its absence
+        :return: the object of the order, according to the specified id or null in case of its absence
         """
-        pass
+        try:
+            return Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return None
 
     def update(self, plated_end_at=None, end_at=None):
         """
@@ -63,21 +122,27 @@ class Order(models.Model):
         :type plated_end_at: int (timestamp)
         :return: None
         """
-        pass
+        if plated_end_at is not None:
+            self.plated_end_at = plated_end_at
+
+        if end_at is not None:
+            self.end_at = end_at
+
+        self.save()
 
     @staticmethod
     def get_all():
         """
         :return: all orders
         """
-        pass
+        return list(Order.objects.all())
 
     @staticmethod
     def get_not_returned_books():
         """
-        :return:  all orders that do not have a return date (end_at)
+        :return: all orders that do not have a return date (end_at)
         """
-        pass
+        return list(Order.objects.filter(end_at=None))
 
     @staticmethod
     def delete_by_id(order_id):
@@ -86,4 +151,9 @@ class Order(models.Model):
         :type order_id: int
         :return: True if object existed in the db and was removed or False if it didn't exist
         """
-        pass
+        try:
+            order = Order.objects.get(id=order_id)
+            order.delete()
+            return True
+        except Order.DoesNotExist:
+            return False
