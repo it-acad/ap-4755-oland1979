70db08c51d37682f5c719d08c811df62a6d3822d
Vladyslav Bondarenko <pertosel1901@gmail.com>
2026-06-07 22:13:50 +0300

    Added views, urls and templates for books and orders

--- library/library/urls.py
+++ library/library/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('users/', include('users.urls')),
     path('authors/', include('author.urls')),
+    path('books/', include('book.urls')),
+    path('orders/', include('order.urls')),
 ]
--- library/order/views.py
+++ library/order/views.py
@@ -1,1 +1,69 @@
-# Create your views here.
+from datetime import timedelta
+
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
+from .models import Order
+from book.models import Book
+
+
+def orders_list(request):
+
+    orders = Order.objects.all()
+
+    return render(
+        request,
+        "order/list.html",
+        {"orders": orders}
+    )
+
+
+def my_orders(request):
+
+    orders = Order.objects.filter(
+        user=request.user
+    )
+
+    return render(
+        request,
+        "order/my_orders.html",
+        {"orders": orders}
+    )
+
+
+def create_order(request, book_id):
+
+    if request.method == "POST":
+
+        book = Book.get_by_id(book_id)
+
+        planned_date = (
+            timezone.now() +
+            timedelta(days=14)
+        )
+
+        Order.create(
+            user=request.user,
+            book=book,
+            plated_end_at=planned_date
+        )
+
+        return redirect("my_orders")
+
+    return redirect("books_list")
+
+
+def close_order(request, id):
+
+    order = Order.get_by_id(id)
+
+    if order:
+
+        order.update(
+            end_at=timezone.now()
+        )
+
+        order.book.count += 1
+        order.book.save()
+
+    return redirect("orders_list")
