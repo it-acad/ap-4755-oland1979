ac68821ea16cee7a1ebcc2d6bbc43f8a0aba64bc
Oland1979 <oland1979@gmail.com>
2026-06-08 23:47:19 +0300

    Update Author model and add migrations

--- library/author/models.py
+++ library/author/models.py
@@ -1,88 +1,50 @@
 from django.db import models
 
-
 class Author(models.Model):
-    """
-        This class represents an Author. \n
-        Attributes:
-        -----------
-        param name: Describes name of the author
-        type name: str max_length=20
-        param surname: Describes last name of the author
-        type surname: str max_length=20
-        param patronymic: Describes middle name of the author
-        type patronymic: str max_length=20
-
-    """
+    # Додаємо поля бази даних
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(max_length=20)
 
     def __str__(self):
-        """
-        Magic method is redefined to show all information about Author.
-        :return: author id, author name, author surname, author patronymic
-        """
+        return f"{self.name} {self.surname} {self.patronymic}"
 
     def __repr__(self):
-        """
-        This magic method is redefined to show class and id of Author object.
-        :return: class, id
-        """
+        return f"Author(id={self.id}, name='{self.name}', surname='{self.surname}')"
 
     @staticmethod
     def get_by_id(author_id):
-        """
-        :param author_id: SERIAL: the id of a Author to be found in the DB
-        :return: author object or None if a user with such ID does not exist
-        """
+        try:
+            return Author.objects.get(id=author_id)
+        except Author.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(author_id):
-        """
-        :param author_id: an id of a author to be deleted
-        :type author_id: int
-        :return: True if object existed in the db and was removed or False if it didn't exist
-        """
+        author = Author.get_by_id(author_id)
+        if author:
+            author.delete()
+            return True
+        return False
 
     @staticmethod
     def create(name, surname, patronymic):
-        """
-        param name: Describes name of the author
-        type name: str max_length=20
-        param surname: Describes surname of the author
-        type surname: str max_length=20
-        param patronymic: Describes patronymic of the author
-        type patronymic: str max_length=20
-        :return: a new author object which is also written into the DB
-        """
+        return Author.objects.create(name=name, surname=surname, patronymic=patronymic)
 
     def to_dict(self):
-        """
-        :return: author id, author name, author surname, author patronymic
-        :Example:
-        | {
-        |   'id': 8,
-        |   'name': 'fn',
-        |   'surname': 'mn',
-        |   'patronymic': 'ln',
-        | }
-        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'surname': self.surname,
+            'patronymic': self.patronymic,
+        }
 
-    def update(self,
-               name=None,
-               surname=None,
-               patronymic=None):
-        """
-        Updates author in the database with the specified parameters.
-        param name: Describes name of the author
-        type name: str max_length=20
-        param surname: Describes surname of the author
-        type surname: str max_length=20
-        param patronymic: Describes patronymic of the author
-        type patronymic: str max_length=20
-        :return: None
-        """
+    def update(self, name=None, surname=None, patronymic=None):
+        if name: self.name = name
+        if surname: self.surname = surname
+        if patronymic: self.patronymic = patronymic
+        self.save()
 
     @staticmethod
     def get_all():
-        """
-        returns data for json request with QuerySet of all authors
-        """
+        return Author.objects.all()
